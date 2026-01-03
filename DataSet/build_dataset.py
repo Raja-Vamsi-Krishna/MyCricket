e7fd02142a7d5d757547688bf7f5f6f121836e10
Vamsi Krishna <kvkrish26@gmail.com>
2026-01-03 14:13:35 +0530

    model class changes

--- DataSet/build_dataset.py
+++ DataSet/build_dataset.py
@@ -12,7 +12,9 @@ CORPUS_PATH = BASE_DIR / "BPE" / "final_corpus.txt"
 OUTPUT_DIR = BASE_DIR / "DataSet" / "Data"
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)   
 TRAIN_RATIO=0.9
-DTYPE=np.uint16
+tokenizer=tiktoken.get_encoding("gpt2")
+
+DTYPE = np.uint16 if tokenizer.n_vocab < 65536 else np.uint32
 
 tokenizer = tiktoken.get_encoding("gpt2")
 
@@ -20,6 +22,8 @@ tokenizer = tiktoken.get_encoding("gpt2")
 vocab_size=tokenizer.n_vocab
 print(vocab_size)
 all_tokens=[]
+text = CORPUS_PATH.read_text(encoding="utf-8")
+assert "<|endoftext|>" not in text, "❌ <|endoftext|> found in corpus!"
 
 with open(CORPUS_PATH,"r",encoding="utf-8") as f:
     for line in tqdm(f,desc="Tokenizing corpus"):
@@ -35,15 +39,16 @@ print(f"Total tokens: {len(all_tokens)}")
 
 split_idx=int(len(all_tokens)*TRAIN_RATIO)
 train_idx=np.array(all_tokens[:split_idx],dtype=DTYPE)
-value_idx=np.array(all_tokens[split_idx:],dtype=DTYPE)
+val_idx=np.array(all_tokens[split_idx:],dtype=DTYPE)
 
 train_idx.tofile(OUTPUT_DIR / "train.bin")
-value_idx.tofile(OUTPUT_DIR / "value.bin")
+val_idx.tofile(OUTPUT_DIR / "val.bin")
 
 meta = {
     "vocab_size": tokenizer.n_vocab,
     "dtype": str(DTYPE),
     "eos_token_id": tokenizer.eot_token,
+    "tokenizer": "tiktoken_gpt2"
 }
 
 with open(OUTPUT_DIR / "meta.json", "w") as f:
